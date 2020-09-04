a264e815863f50a71805336cdfefa97445550741
Fly Chang <leo424y@gmail.com>
2020-09-04 15:07:58 +0800

    table with uuid

---
+++ app/models/dtl.rb
@@ -0,0 +1,6 @@
+class Dtl < ApplicationRecord
+  before_save :set_uuid 
+  def set_uuid
+    self.uuid = SecureRandom.uuid
+  end
+end
---
+++ db/migrate/20200904070433_create_dtls.rb
@@ -0,0 +1,26 @@
+class CreateDtls < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dtls do |t|
+      t.string :source
+      t.uuid :uuid
+      t.string :url
+      t.string :platform_id
+      t.string :link
+      t.string :domain
+      t.string :channel_id
+      t.string :channel_name
+      t.string :creator_id
+      t.string :creator_name
+      t.string :title
+      t.string :discription
+      t.text :content
+      t.jsonb :media_meta
+      t.jsonb :system_meta
+      t.datetime :pub_time
+
+      t.timestamps
+    end
+  end
+end
+
+

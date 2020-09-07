cbd79dc2687f6d0c3f415a6345f5fdbe1a04b1d1
Fly Chang <leo424y@gmail.com>
2020-09-07 14:57:31 +0800

    rename func

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,13 +3,13 @@ module Types
     description "The query root of this schema"
 
     # First describe the field signature:
-    field :dtl, DtlType, null: true do
+    field :get_dtl, DtlType, null: true do
       description "Find a Dtl by ID"
       argument :id, ID, required: true
     end
   
     # Then provide an implementation:
-    def dtl(id:)
+    def get_dtl(id:)
       Dtl.find(id)
     end
   end

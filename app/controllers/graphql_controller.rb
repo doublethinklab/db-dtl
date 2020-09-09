8fc659cfbf33f32448cf1dd91518e3dce6091bba
Fly Chang <leo424y@gmail.com>
2020-09-09 14:35:18 +0800

    enhence controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,12 +6,12 @@ class GraphqlController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def execute
-    variables = prepare_variables(params[:variables])
+    variables = prepare_variables(ensure_hash params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      session: session,
+      current_user: AuthToken.user_from_token(session[:token])
     }
     result = DbDtlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -39,6 +39,23 @@ class GraphqlController < ApplicationController
       {}
     else
       raise ArgumentError, "Unexpected parameter: #{variables_param}"
+    end
+  end
+
+  def ensure_hash(ambiguous_param)
+    case ambiguous_param
+    when String
+      if ambiguous_param.present?
+        ensure_hash(JSON.parse(ambiguous_param))
+      else
+        {}
+      end
+    when Hash, ActionController::Parameters
+      ambiguous_param
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
     end
   end
 

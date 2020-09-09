34f783b265aa9127687cac6c210011801d4a238e
Fly Chang <leo424y@gmail.com>
2020-09-09 13:44:49 +0800

    ✨add user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_dtl, mutation: Mutations::CreateDtl
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_070433) do
+ActiveRecord::Schema.define(version: 2020_09_09_052045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2020_09_04_070433) do
     t.jsonb "media_meta"
     t.jsonb "system_meta"
     t.datetime "pub_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

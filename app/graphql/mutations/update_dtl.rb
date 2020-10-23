ea845a11f17108dce7725f0dfa80abd5e62d5c53
Fly Chang <leo424y@gmail.com>
2020-10-23 12:14:25 +0800

    ✨ update API

---
+++ app/graphql/mutations/update_dtl.rb
@@ -0,0 +1,38 @@
+class Mutations::UpdateDtl < Mutations::BaseMutation
+  argument :url, String, required: true
+  argument :title, String, required: false
+  argument :content, String, required: false
+  argument :description, String, required: false
+  argument :pub_time, String, required: false
+
+  field :dtl, Types::DtlType, null: false
+  field :errors, [String], null: false
+
+  def resolve(
+    url: nil,
+    title: '',
+    description: '',
+    content: '',
+    pub_time: nil
+  )
+    dtl = Dtl.find_by(url: url)
+    dtl.update!(
+      title: title.present? ? URI.decode_www_form_component(title) : dtl.title,
+      description: description.present? ? URI.decode_www_form_component(description) : dtl.description,
+      content: content.present? ? URI.decode_www_form_component(content) : dtl.content,
+      pub_time: pub_time || dtl.pub_time
+    )
+
+    if dtl
+      {
+        dtl: dtl,
+        errors: []
+      }
+    else
+      {
+        dtl: nil,
+        errors: order.errors.full_messages
+      }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_dtl, mutation: Mutations::CreateDtl
+    field :update_dtl, mutation: Mutations::UpdateDtl
     field :create_user, mutation: Mutations::CreateUser
     field :create_label, mutation: Mutations::CreateLabel
     field :create_document_token_count, mutation: Mutations::CreateDocumentTokenCount

bbc92af823460e56b9675a283cffd4eabe3e9110
Fly Chang <leo424y@gmail.com>
2020-12-22 13:53:15 +0800

    update original source for dtl

--- app/graphql/mutations/update_dtl.rb
+++ app/graphql/mutations/update_dtl.rb
@@ -5,6 +5,9 @@ class Mutations::UpdateDtl < Mutations::BaseMutation
   argument :description, String, required: false
   argument :pub_time, String, required: false
   argument :language, String, required: false
+  argument :original_source_name, String, required: false
+  argument :original_source_url, String, required: false
+  argument :original_source_check, String, required: false
 
   field :dtl, Types::DtlType, null: false
   field :errors, [String], null: false
@@ -14,14 +17,20 @@ class Mutations::UpdateDtl < Mutations::BaseMutation
     title: '',
     description: '',
     content: '',
-    pub_time: nil
+    pub_time: nil,
+    original_source_name: nil,
+    original_source_url: nil,
+    original_source_check: nil
   )
     dtl = Dtl.find_by(url: url)
     dtl.update!(
       title: title.present? ? URI.decode_www_form_component(title) : dtl.title,
       description: description.present? ? URI.decode_www_form_component(description) : dtl.description,
       content: content.present? ? URI.decode_www_form_component(content) : dtl.content,
-      pub_time: pub_time || dtl.pub_time
+      pub_time: pub_time || dtl.pub_time,
+      original_source_name: original_source_name,
+      original_source_url: original_source_url,
+      original_source_check: original_source_check
     )
 
     if dtl

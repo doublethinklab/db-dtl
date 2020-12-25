b2fd64e7b0b58b4529e688e7206913bd529e74ba
Fly Chang <leo424y@gmail.com>
2020-12-25 11:01:14 +0800

    uuid bug

--- app/models/dtl.rb
+++ app/models/dtl.rb
@@ -1,7 +1,7 @@
 class Dtl < ApplicationRecord
   validates :url, uniqueness: true, presence: true
 
-  before_save :set_uuid
+  before_create :set_uuid
   def set_uuid
     self.uuid = SecureRandom.uuid
   end
